185684c99f628674cb3da3870f70a04c70d5d7cf
Jcvdm <vandermerwe.jaco194@gmail.com>
2025-08-23 09:54:34 +0200

    chore(bookings): centralize Fresha bookingUrl in src/config.ts and update all CTAs to use it

---
+++ src/config.ts
@@ -0,0 +1,6 @@
+// Centralized configuration for ARJAY Beauty Lounge
+// Update here to change the booking URL everywhere
+
+export const bookingUrl =
+  'https://www.fresha.com/book-now/nails-by-riana-o5775nr5/all-offer?share=true&pId=371323';
+
